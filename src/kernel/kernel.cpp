a5ccb55f63d44a5318e278ed2c6eaa6283deb229
James Osborne <james.osborne112@gmail.com>
2019-02-22 15:06:31 -0500

    Fix main() name

--- src/kernel/kernel.cpp
+++ src/kernel/kernel.cpp
@@ -22,7 +22,7 @@
  * Panix kernel main function. Called in kernel_entry.asm
  * Executed prior to bootloader code in boot32.asm
  */
-int main() {
+extern "C" int kernelMain() {
     isrInstall();
     irqInstall();
 
@@ -51,7 +51,22 @@ void handleUserInput(char *input) {
     if (stringComparison(input, (char*) "END") == 0) {
         kprint((char*) "Stopping the CPU. Bye!\n");
         asm volatile("hlt");
-    }
+    } else if (stringComparison(input, (char*) "PAGE") == 0) {
+        uint32_t physicalAddress;
+        uint32_t page = kmalloc(1000, 1, &physicalAddress);
+        
+        char pageHexString[16] = "";
+        hexToString(page, pageHexString);
+
+        char physicalAddressHexString[16] = "";
+        hexToString(physicalAddress, physicalAddressHexString);
+        
+        kprint((char*) "Page: ");
+        kprint(pageHexString);
+        kprint((char*) ", physical address: ");
+        kprint(physicalAddressHexString);
+        kprint((char*) "\n");
+}
     kprint((char*) "You said: ");
     kprint(input);
     kprint((char*) "\n> ");
