9fdd963813ff8f58c1b9a484ec5e7f2a01d18a8c
James Osborne <james.osborne112@gmail.com>
2019-02-22 00:19:19 -0500

    Add irq externs and registration

--- src/cpu/isr.h
+++ src/cpu/isr.h
@@ -3,8 +3,8 @@
 
 #include "types.h"
 
-/* ISRs reserved for CPU exceptions */
 extern "C" {
+    /* ISRs reserved for CPU exceptions */
     void isr0();
     void isr1();
     void isr2();
@@ -37,7 +37,42 @@ extern "C" {
     void isr29();
     void isr30();
     void isr31();
+
+    /* IRQ definitions */
+    void irq0();
+    void irq1();
+    void irq2();
+    void irq3();
+    void irq4();
+    void irq5();
+    void irq6();
+    void irq7();
+    void irq8();
+    void irq9();
+    void irq10();
+    void irq11();
+    void irq12();
+    void irq13();
+    void irq14();
+    void irq15();
 }
+
+#define IRQ0 32
+#define IRQ1 33
+#define IRQ2 34
+#define IRQ3 35
+#define IRQ4 36
+#define IRQ5 37
+#define IRQ6 38
+#define IRQ7 39
+#define IRQ8 40
+#define IRQ9 41
+#define IRQ10 42
+#define IRQ11 43
+#define IRQ12 44
+#define IRQ13 45
+#define IRQ14 46
+#define IRQ15 47
 
 /* Struct which aggregates many registers */
 typedef struct {
@@ -67,6 +102,12 @@ typedef struct {
 } registers_t;
 
 void isrInstall();
-extern "C" void isrHandler(registers_t r);
+extern "C" {
+    void irqHandler(registers_t r);
+    void isrHandler(registers_t r);
+}
+
+typedef void (*isr_t)(registers_t);
+void registerInterruptHandler(uint8_t n, isr_t handler);
 
 #endif
