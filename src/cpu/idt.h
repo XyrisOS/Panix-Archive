c07c4bad37aafdd4c3615481513730c0d9168187
James Osborne <james.osborne112@gmail.com>
2019-02-22 00:18:17 -0500

    Add comment about inline variables

--- src/cpu/idt.h
+++ src/cpu/idt.h
@@ -28,9 +28,9 @@ typedef struct {
 } __attribute__((packed)) idt_register_t;
 
 #define IDT_ENTRIES 256
+/* `inline` prevents a multiple definition error */
 inline idt_gate_t idt[IDT_ENTRIES];
 inline idt_register_t idtRegister;
-
 
 /* Functions implemented in idt.c */
 void setIdtGate(int n, uint32_t handler);
