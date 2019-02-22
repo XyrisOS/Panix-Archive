8e9684519909780b87bc6ff80a496d6edc715921
James Osborne <james.osborne112@gmail.com>
2019-02-22 00:18:58 -0500

    Add char* casts and irq setup

--- src/cpu/isr.cpp
+++ src/cpu/isr.cpp
@@ -1,7 +1,48 @@
 #include "isr.h"
 #include "idt.h"
+#include "../drivers/ports.h"
 #include "../drivers/screen.h"
 #include "../kernel/util/util.h"
+
+isr_t interruptHandlers[256];
+/* To print the message which defines every exception */
+char* exceptionMessages[] = {
+    (char*) "Division By Zero",
+    (char*) "Debug",
+    (char*) "Non Maskable Interrupt",
+    (char*) "Breakpoint",
+    (char*) "Into Detected Overflow",
+    (char*) "Out of Bounds",
+    (char*) "Invalid Opcode",
+    (char*) "No Coprocessor",
+
+    (char*) "Double Fault",
+    (char*) "Coprocessor Segment Overrun",
+    (char*) "Bad TSS",
+    (char*) "Segment Not Present",
+    (char*) "Stack Fault",
+    (char*) "General Protection Fault",
+    (char*) "Page Fault",
+    (char*) "Unknown Interrupt",
+
+    (char*) "Coprocessor Fault",
+    (char*) "Alignment Check",
+    (char*) "Machine Check",
+    (char*) "Reserved",
+    (char*) "Reserved",
+    (char*) "Reserved",
+    (char*) "Reserved",
+    (char*) "Reserved",
+
+    (char*) "Reserved",
+    (char*) "Reserved",
+    (char*) "Reserved",
+    (char*) "Reserved",
+    (char*) "Reserved",
+    (char*) "Reserved",
+    (char*) "Reserved",
+    (char*) "Reserved"
+};
 
 /* Can't do this with a loop because we need the address
  * of the function names */
@@ -39,54 +80,67 @@ void isrInstall() {
     setIdtGate(30, (uint32_t)isr30);
     setIdtGate(31, (uint32_t)isr31);
 
-    setIdt(); // Load with ASM
+    /* Remap the PIC */
+    port_byte_out(0x20, 0x11);
+    port_byte_out(0xA0, 0x11);
+    port_byte_out(0x21, 0x20);
+    port_byte_out(0xA1, 0x28);
+    port_byte_out(0x21, 0x04);
+    port_byte_out(0xA1, 0x02);
+    port_byte_out(0x21, 0x01);
+    port_byte_out(0xA1, 0x01);
+    port_byte_out(0x21, 0x0);
+    port_byte_out(0xA1, 0x0); 
+
+    /* Install the IRQs */
+    setIdtGate(32, (uint32_t)irq0);
+    setIdtGate(33, (uint32_t)irq1);
+    setIdtGate(34, (uint32_t)irq2);
+    setIdtGate(35, (uint32_t)irq3);
+    setIdtGate(36, (uint32_t)irq4);
+    setIdtGate(37, (uint32_t)irq5);
+    setIdtGate(38, (uint32_t)irq6);
+    setIdtGate(39, (uint32_t)irq7);
+    setIdtGate(40, (uint32_t)irq8);
+    setIdtGate(41, (uint32_t)irq9);
+    setIdtGate(42, (uint32_t)irq10);
+    setIdtGate(43, (uint32_t)irq11);
+    setIdtGate(44, (uint32_t)irq12);
+    setIdtGate(45, (uint32_t)irq13);
+    setIdtGate(46, (uint32_t)irq14);
+    setIdtGate(47, (uint32_t)irq15);
+
+    setIdt(); /* Load with ASM */
 }
 
-/* To print the message which defines every exception */
-char* exceptionMessages[] = {
-    "Division By Zero",
-    "Debug",
-    "Non Maskable Interrupt",
-    "Breakpoint",
-    "Into Detected Overflow",
-    "Out of Bounds",
-    "Invalid Opcode",
-    "No Coprocessor",
-
-    "Double Fault",
-    "Coprocessor Segment Overrun",
-    "Bad TSS",
-    "Segment Not Present",
-    "Stack Fault",
-    "General Protection Fault",
-    "Page Fault",
-    "Unknown Interrupt",
-
-    "Coprocessor Fault",
-    "Alignment Check",
-    "Machine Check",
-    "Reserved",
-    "Reserved",
-    "Reserved",
-    "Reserved",
-    "Reserved",
-
-    "Reserved",
-    "Reserved",
-    "Reserved",
-    "Reserved",
-    "Reserved",
-    "Reserved",
-    "Reserved",
-    "Reserved"
-};
-
 void isrHandler(registers_t r) {
-    kprint("received interrupt: ");
+    kprint((char*) "received interrupt: ");
     char s[3];
     int_to_ascii(r.interruptNumber, s);
     kprint(s);
-    kprint("\n");
+    kprint((char*) "\n");
     kprint(exceptionMessages[r.interruptNumber]);
-    kprint("\n");
+    kprint((char*) "\n");
+}
+
+void registerInterruptHandler(uint8_t n, isr_t handler) {
+    interruptHandlers[n] = handler;
+}
+
+void irqHandler(registers_t r) {
+    /* After every interrupt we need to send an EOI to the PICs
+     * or they will not send another interrupt again */
+    
+    /* slave */
+    if (r.interruptNumber >= 40) {
+        port_byte_out(0xA0, 0x20);
+    }
+    /* master */
+    port_byte_out(0x20, 0x20); 
+
+    /* Handle the interrupt in a more modular way */
+    if (interruptHandlers[r.interruptNumber] != 0) {
+        isr_t handler = interruptHandlers[r.interruptNumber];
+        handler(r);
+    }
 }
