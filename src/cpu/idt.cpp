e2cd1a7e9f789fe6266d5045b8be194521f37a01
James Osborne <james.osborne112@gmail.com>
2019-02-22 15:12:43 -0500

    Remove unnecessary underscores on asm and volatile

--- src/cpu/idt.cpp
+++ src/cpu/idt.cpp
@@ -12,5 +12,5 @@ void setIdt() {
     idtRegister.base = (uint32_t) &idt;
     idtRegister.limit = IDT_ENTRIES * sizeof(idt_gate_t) - 1;
     /* Don't make the mistake of loading &idt -- always load &idt_reg */
-    __asm__ __volatile__("lidtl (%0)" : : "r" (&idtRegister));
+    asm volatile("lidtl (%0)" : : "r" (&idtRegister));
 }
