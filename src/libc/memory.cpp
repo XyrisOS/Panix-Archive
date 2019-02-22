ee292a3f8a84343abcb7b4121a43d8c73b4f7cb4
James Osborne <james.osborne112@gmail.com>
2019-02-22 15:06:42 -0500

    Add kmalloc

--- src/libc/memory.cpp
+++ src/libc/memory.cpp
@@ -24,3 +24,26 @@ void memorySet(uint8_t* destination, uint8_t value, uint32_t length) {
         --length;
     }
 }
+
+/* This should be computed at link time, but a hardcoded
+ * value is fine for now. Remember that our kernel starts
+ * at 0x1000 as defined on the Makefile */
+uint32_t freeMemoryAddress = 0x10000;
+/* Implementation is just a pointer to some free memory which
+ * keeps growing */
+uint32_t kmalloc(uint32_t size, int align, uint32_t* physicalAddress) {
+    /* Pages are aligned to 4K, or 0x1000 */
+    if (align == 1 && (freeMemoryAddress & 0xFFFFF000)) {
+        freeMemoryAddress &= 0xFFFFF000;
+        freeMemoryAddress += 0x1000;
+    }
+    /* Save also the physical address */
+    if (physicalAddress) {
+        *physicalAddress = freeMemoryAddress;
+    }
+
+    uint32_t result = freeMemoryAddress;
+    freeMemoryAddress += size; /* Remember to increment the pointer */
+
+    return result;
+}
--- src/libc/memory.h
+++ src/libc/memory.h
@@ -18,4 +18,9 @@ void memoryCopy(char* source, char* destination, int nbytes);
  */
 void memorySet(uint8_t* destination, uint8_t value, uint32_t length);
 
+/**
+ * At this stage there is no 'free' implemented.
+ */
+uint32_t kmalloc(uint32_t size, int align, uint32_t* physicalAddress);
+
 #endif /* MEMORY_H */
