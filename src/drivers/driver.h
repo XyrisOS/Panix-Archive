0ba5e4aa5e8a630913334943663e557c7b088e78
James Osborne <james.osborne112@gmail.com>
2019-02-23 17:38:25 -0500

    Create base Driver class

---
+++ src/drivers/driver.h
@@ -0,0 +1,13 @@
+#ifndef DRIVER_H
+#define DRIVER_H
+
+class Driver {
+    public:
+        /* Constructors */
+        Driver() {}
+
+        /* Destructors */
+        virtual ~Driver() {}
+};
+
+#endif /* DRIVER_H */
