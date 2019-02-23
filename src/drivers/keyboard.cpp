593e29e3b6877845d6c10d2318f8862d52b10f8a
James Osborne <james.osborne112@gmail.com>
2019-02-23 17:39:10 -0500

    Create Keybaord class. All functions are currently static because this will never need multiple instances

--- src/drivers/keyboard.cpp
+++ src/drivers/keyboard.cpp
@@ -1,8 +1,27 @@
 #include "keyboard.h"
 
-void printLetter(uint8_t scancode);
+const char Keyboard::scancodeAscii[] = { 
+    '?', '?', '1', '2', '3', '4', '5', '6',
+    '7', '8', '9', '0', '-', '=', '?', '?',
+    'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I',
+    'O', 'P', '[', ']', '?', '?', 'A', 'S',
+    'D', 'F', 'G', 'H', 'J', 'K', 'L', ';',
+    '\'', '`', '?', '\\', 'Z', 'X', 'C', 'V',
+    'B', 'N', 'M', ',', '.', '/', '?', '?',
+    '?', ' '
+};
+const char* Keyboard::scancodeName[] = {
+    "ERROR", "Esc", "1", "2", "3", "4", "5", "6",
+    "7", "8", "9", "0", "-", "=", "Backspace", "Tab",
+    "Q", "W", "E", "R", "T", "Y", "U", "I",
+    "O", "P", "[", "]", "Enter", "Lctrl", "A", "S",
+    "D", "F", "G", "H", "J", "K", "L", ";",
+    "'", "`", "LShift", "\\", "Z", "X", "C", "V",
+    "B", "N", "M", ",", ".", "/", "RShift", "Keypad *",
+    "LAlt", "Spacebar"
+};
 
-static void keyboardCallback(registers_t regs) {
+void Keyboard::callback(registers_t regs) {
     /* The PIC leaves us the scancode in port 0x60 */
     uint8_t scancode = getPortByte(0x60);
     
@@ -10,22 +29,22 @@ static void keyboardCallback(registers_t regs) {
         return;
     }
     if (scancode == BACKSPACE) {
-        backspace(keyBuffer);
+        backspace(Keyboard::keyBuffer);
         kprintBackspace();
     } else if (scancode == ENTER) {
         kprint((char*) "\n");
-        handleUserInput(keyBuffer);
-        keyBuffer[0] = '\0';
+        handleUserInput(Keyboard::keyBuffer);
+        Keyboard::keyBuffer[0] = '\0';
     } else {
         char letter = scancodeAscii[(int) scancode];
         /* Remember that kprint only accepts char[] */
         char str[2] = {letter, '\0'};
-        append(keyBuffer, letter);
+        append(Keyboard::keyBuffer, letter);
         kprint(str);
     }
     UNUSED(regs);
 }
 
-void initKeyboard() {
-   registerInterruptHandler(IRQ1, keyboardCallback); 
+void Keyboard::initialize() {
+   registerInterruptHandler(IRQ1, Keyboard::callback); 
 }
--- src/drivers/keyboard.h
+++ src/drivers/keyboard.h
@@ -6,52 +6,44 @@
 #include "../kernel/kernel.h"
 #include "../libc/function.h"
 #include "../libc/string.h"
+#include "driver.h"
 #include "screen.h"
 
 #define BACKSPACE 0x0E
 #define ENTER 0x1C
 #define SCANCODE_MAX 57
 
-/**
- * @brief 
- * 
- */
-void initKeyboard();
+class Keyboard : public Driver {
+    public:
+        /**
+         * @brief 
+         * 
+         */
+        static const char* scancodeName[];
 
-/**
- * @brief 
- * 
- */
-inline char keyBuffer[256];
+        /**
+         * @brief 
+         * 
+         */
+        static const char scancodeAscii[];
 
-/**
- * @brief 
- * 
- */
-inline const char* scancodeName[] = {
-    "ERROR", "Esc", "1", "2", "3", "4", "5", "6", 
-    "7", "8", "9", "0", "-", "=", "Backspace", "Tab",
-    "Q", "W", "E", "R", "T", "Y", "U", "I",
-    "O", "P", "[", "]", "Enter", "Lctrl", "A", "S",
-    "D", "F", "G", "H", "J", "K", "L", ";",
-    "'", "`", "LShift", "\\", "Z", "X", "C", "V",
-    "B", "N", "M", ",", ".", "/", "RShift", "Keypad *",
-    "LAlt", "Spacebar"
-};
-
-/**
- * @brief 
- * 
- */
-const char scancodeAscii[] = { 
-    '?', '?', '1', '2', '3', '4', '5', '6',     
-    '7', '8', '9', '0', '-', '=', '?', '?',
-    'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I',
-    'O', 'P', '[', ']', '?', '?', 'A', 'S',
-    'D', 'F', 'G', 'H', 'J', 'K', 'L', ';',
-    '\'', '`', '?', '\\', 'Z', 'X', 'C', 'V', 
-    'B', 'N', 'M', ',', '.', '/', '?', '?',
-    '?', ' '
+        /**
+         * @brief 
+         * 
+         */
+        static void callback(registers_t regs);
+        /**
+         * @brief 
+         * 
+         */
+        static void initialize();
+    
+    private:
+        /**
+         * @brief 
+         * 
+         */
+        inline static char keyBuffer[256];
 };
 
 #endif /* KEYBOARD_H */
