741cd0c157ba7f46ce3b6dbfec3cab1f91e8ce4e
James Osborne <james.osborne112@gmail.com>
2019-02-23 20:00:06 -0500

    Create colors.h

---
+++ src/drivers/screen/colors.h
@@ -0,0 +1,13 @@
+/**
+ * File: colors.h
+ * Author: Keeton Feavel and James Osborne
+ */
+
+#ifndef COLORS_H
+#define COLORS_H
+
+#define WHITE_ON_BLACK 0x0f
+#define RED_ON_WHITE 0xf4
+#define YELLOW_ON_BLACK 0x0e
+
+#endif /* COLORS_H */
--- src/drivers/screen/screen.h
+++ src/drivers/screen/screen.h
@@ -1,6 +1,6 @@
 /**
  * File: screen.h
- * Author: Keeton Feavel
+ * Author: Keeton Feavel and James Osborne
  */
 
 #ifndef SCREEN_H
@@ -9,14 +9,11 @@
 #include "../../cpu/ports.h"
 #include "../../libc/memory.h"
 #include "../driver.h"
+#include "colors.h"
 
 #define VIDEO_ADDRESS 0xb8000
 #define MAX_ROWS 25
 #define MAX_COLUMNS 80
-// TODO: Move to separate 'colors.h' file like in edix_v1
-#define WHITE_ON_BLACK 0x0f
-#define RED_ON_WHITE 0xf4
-#define YELLOW_ON_BLACK 0x0e
 
 /* Screen i/o ports */
 #define REG_SCREEN_CTRL 0x3d4
