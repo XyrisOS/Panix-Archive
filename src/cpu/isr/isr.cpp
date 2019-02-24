81ff76b1beb9355f7a3fef1d6f541022742c9fe7
James Osborne <james.osborne112@gmail.com>
2019-02-23 22:24:23 -0500

    Fix exception messages and interrupt printing

--- src/cpu/isr/isr.cpp
+++ src/cpu/isr/isr.cpp
@@ -7,42 +7,42 @@
 
 isr_t interruptHandlers[256];
 /* To print the message which defines every exception */
-char* exceptionMessages[] = {
-    (char*) "Division By Zero",
-    (char*) "Debug",
-    (char*) "Non Maskable Interrupt",
-    (char*) "Breakpoint",
-    (char*) "Into Detected Overflow",
-    (char*) "Out of Bounds",
-    (char*) "Invalid Opcode",
-    (char*) "No Coprocessor",
+const char exceptionMessages[32][32] {
+    "Division By Zero",
+    "Debug",
+    "Non Maskable Interrupt",
+    "Breakpoint",
+    "Into Detected Overflow",
+    "Out of Bounds",
+    "Invalid Opcode",
+    "No Coprocessor",
 
-    (char*) "Double Fault",
-    (char*) "Coprocessor Segment Overrun",
-    (char*) "Bad TSS",
-    (char*) "Segment Not Present",
-    (char*) "Stack Fault",
-    (char*) "General Protection Fault",
-    (char*) "Page Fault",
-    (char*) "Unknown Interrupt",
+    "Double Fault",
+    "Coprocessor Segment Overrun",
+    "Bad TSS",
+    "Segment Not Present",
+    "Stack Fault",
+    "General Protection Fault",
+    "Page Fault",
+    "Unknown Interrupt",
 
-    (char*) "Coprocessor Fault",
-    (char*) "Alignment Check",
-    (char*) "Machine Check",
-    (char*) "Reserved",
-    (char*) "Reserved",
-    (char*) "Reserved",
-    (char*) "Reserved",
-    (char*) "Reserved",
+    "Coprocessor Fault",
+    "Alignment Check",
+    "Machine Check",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
 
-    (char*) "Reserved",
-    (char*) "Reserved",
-    (char*) "Reserved",
-    (char*) "Reserved",
-    (char*) "Reserved",
-    (char*) "Reserved",
-    (char*) "Reserved",
-    (char*) "Reserved"
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved"
 };
 
 void irqInstall() {
@@ -57,38 +57,38 @@ void irqInstall() {
 /* Can't do this with a loop because we need the address
  * of the function names */
 void isrInstall() {
-    setIdtGate(0, (uint32_t)isr0);
-    setIdtGate(1, (uint32_t)isr1);
-    setIdtGate(2, (uint32_t)isr2);
-    setIdtGate(3, (uint32_t)isr3);
-    setIdtGate(4, (uint32_t)isr4);
-    setIdtGate(5, (uint32_t)isr5);
-    setIdtGate(6, (uint32_t)isr6);
-    setIdtGate(7, (uint32_t)isr7);
-    setIdtGate(8, (uint32_t)isr8);
-    setIdtGate(9, (uint32_t)isr9);
-    setIdtGate(10, (uint32_t)isr10);
-    setIdtGate(11, (uint32_t)isr11);
-    setIdtGate(12, (uint32_t)isr12);
-    setIdtGate(13, (uint32_t)isr13);
-    setIdtGate(14, (uint32_t)isr14);
-    setIdtGate(15, (uint32_t)isr15);
-    setIdtGate(16, (uint32_t)isr16);
-    setIdtGate(17, (uint32_t)isr17);
-    setIdtGate(18, (uint32_t)isr18);
-    setIdtGate(19, (uint32_t)isr19);
-    setIdtGate(20, (uint32_t)isr20);
-    setIdtGate(21, (uint32_t)isr21);
-    setIdtGate(22, (uint32_t)isr22);
-    setIdtGate(23, (uint32_t)isr23);
-    setIdtGate(24, (uint32_t)isr24);
-    setIdtGate(25, (uint32_t)isr25);
-    setIdtGate(26, (uint32_t)isr26);
-    setIdtGate(27, (uint32_t)isr27);
-    setIdtGate(28, (uint32_t)isr28);
-    setIdtGate(29, (uint32_t)isr29);
-    setIdtGate(30, (uint32_t)isr30);
-    setIdtGate(31, (uint32_t)isr31);
+    setIdtGate(0, (uint32_t) isr0);
+    setIdtGate(1, (uint32_t) isr1);
+    setIdtGate(2, (uint32_t) isr2);
+    setIdtGate(3, (uint32_t) isr3);
+    setIdtGate(4, (uint32_t) isr4);
+    setIdtGate(5, (uint32_t) isr5);
+    setIdtGate(6, (uint32_t) isr6);
+    setIdtGate(7, (uint32_t) isr7);
+    setIdtGate(8, (uint32_t) isr8);
+    setIdtGate(9, (uint32_t) isr9);
+    setIdtGate(10, (uint32_t) isr10);
+    setIdtGate(11, (uint32_t) isr11);
+    setIdtGate(12, (uint32_t) isr12);
+    setIdtGate(13, (uint32_t) isr13);
+    setIdtGate(14, (uint32_t) isr14);
+    setIdtGate(15, (uint32_t) isr15);
+    setIdtGate(16, (uint32_t) isr16);
+    setIdtGate(17, (uint32_t) isr17);
+    setIdtGate(18, (uint32_t) isr18);
+    setIdtGate(19, (uint32_t) isr19);
+    setIdtGate(20, (uint32_t) isr20);
+    setIdtGate(21, (uint32_t) isr21);
+    setIdtGate(22, (uint32_t) isr22);
+    setIdtGate(23, (uint32_t) isr23);
+    setIdtGate(24, (uint32_t) isr24);
+    setIdtGate(25, (uint32_t) isr25);
+    setIdtGate(26, (uint32_t) isr26);
+    setIdtGate(27, (uint32_t) isr27);
+    setIdtGate(28, (uint32_t) isr28);
+    setIdtGate(29, (uint32_t) isr29);
+    setIdtGate(30, (uint32_t) isr30);
+    setIdtGate(31, (uint32_t) isr31);
 
     /* Remap the PIC */
     setPortByte(0x20, 0x11);
@@ -124,13 +124,12 @@ void isrInstall() {
 }
 
 void isrHandler(registers_t r) {
-    Screen::kprint((char*) "received interrupt: ");
-    char s[3];
-    intToString(r.interruptNumber, s);
-    Screen::kprint(s);
+    /* TODO: Write a panic message. Maybe animate it? */
+    Screen::clearScreen();
     Screen::kprint((char*) "\n");
-    Screen::kprint(exceptionMessages[r.interruptNumber]);
+    Screen::kprint((char*) exceptionMessages[r.interruptNumber]);
     Screen::kprint((char*) "\n");
+    asm volatile("hlt");
 }
 
 void registerInterruptHandler(uint8_t n, isr_t handler) {
