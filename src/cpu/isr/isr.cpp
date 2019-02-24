18d504a14c7fc7727f2aeb0389ef1ae13899a5af
James Osborne <james.osborne112@gmail.com>
2019-02-23 23:08:57 -0500

    Update references to IDTs

--- src/cpu/isr/isr.cpp
+++ src/cpu/isr/isr.cpp
@@ -69,38 +69,38 @@ void irqInstall() {
 /* Can't do this with a loop because we need the address
  * of the function names */
 void isrInstall() {
-    setIdtGate(0, (uint32_t) isr0);
-    setIdtGate(1, (uint32_t) isr1);
-    setIdtGate(2, (uint32_t) isr2);
-    setIdtGate(3, (uint32_t) isr3);
-    setIdtGate(4, (uint32_t) isr4);
-    setIdtGate(5, (uint32_t) isr5);
-    setIdtGate(6, (uint32_t) isr6);
-    setIdtGate(7, (uint32_t) isr7);
-    setIdtGate(8, (uint32_t) isr8);
-    setIdtGate(9, (uint32_t) isr9);
-    setIdtGate(10, (uint32_t) isr10);
-    setIdtGate(11, (uint32_t) isr11);
-    setIdtGate(12, (uint32_t) isr12);
-    setIdtGate(13, (uint32_t) isr13);
-    setIdtGate(14, (uint32_t) isr14);
-    setIdtGate(15, (uint32_t) isr15);
-    setIdtGate(16, (uint32_t) isr16);
-    setIdtGate(17, (uint32_t) isr17);
-    setIdtGate(18, (uint32_t) isr18);
-    setIdtGate(19, (uint32_t) isr19);
-    setIdtGate(20, (uint32_t) isr20);
-    setIdtGate(21, (uint32_t) isr21);
-    setIdtGate(22, (uint32_t) isr22);
-    setIdtGate(23, (uint32_t) isr23);
-    setIdtGate(24, (uint32_t) isr24);
-    setIdtGate(25, (uint32_t) isr25);
-    setIdtGate(26, (uint32_t) isr26);
-    setIdtGate(27, (uint32_t) isr27);
-    setIdtGate(28, (uint32_t) isr28);
-    setIdtGate(29, (uint32_t) isr29);
-    setIdtGate(30, (uint32_t) isr30);
-    setIdtGate(31, (uint32_t) isr31);
+    IDT::setIdtGate(0, (uint32_t) isr0);
+    IDT::setIdtGate(1, (uint32_t) isr1);
+    IDT::setIdtGate(2, (uint32_t) isr2);
+    IDT::setIdtGate(3, (uint32_t) isr3);
+    IDT::setIdtGate(4, (uint32_t) isr4);
+    IDT::setIdtGate(5, (uint32_t) isr5);
+    IDT::setIdtGate(6, (uint32_t) isr6);
+    IDT::setIdtGate(7, (uint32_t) isr7);
+    IDT::setIdtGate(8, (uint32_t) isr8);
+    IDT::setIdtGate(9, (uint32_t) isr9);
+    IDT::setIdtGate(10, (uint32_t) isr10);
+    IDT::setIdtGate(11, (uint32_t) isr11);
+    IDT::setIdtGate(12, (uint32_t) isr12);
+    IDT::setIdtGate(13, (uint32_t) isr13);
+    IDT::setIdtGate(14, (uint32_t) isr14);
+    IDT::setIdtGate(15, (uint32_t) isr15);
+    IDT::setIdtGate(16, (uint32_t) isr16);
+    IDT::setIdtGate(17, (uint32_t) isr17);
+    IDT::setIdtGate(18, (uint32_t) isr18);
+    IDT::setIdtGate(19, (uint32_t) isr19);
+    IDT::setIdtGate(20, (uint32_t) isr20);
+    IDT::setIdtGate(21, (uint32_t) isr21);
+    IDT::setIdtGate(22, (uint32_t) isr22);
+    IDT::setIdtGate(23, (uint32_t) isr23);
+    IDT::setIdtGate(24, (uint32_t) isr24);
+    IDT::setIdtGate(25, (uint32_t) isr25);
+    IDT::setIdtGate(26, (uint32_t) isr26);
+    IDT::setIdtGate(27, (uint32_t) isr27);
+    IDT::setIdtGate(28, (uint32_t) isr28);
+    IDT::setIdtGate(29, (uint32_t) isr29);
+    IDT::setIdtGate(30, (uint32_t) isr30);
+    IDT::setIdtGate(31, (uint32_t) isr31);
 
     /* Remap the PIC */
     setPortByte(0x20, 0x11);
@@ -115,24 +115,24 @@ void isrInstall() {
     setPortByte(0xA1, 0x0); 
 
     /* Install the IRQs */
-    setIdtGate(32, (uint32_t)irq0);
-    setIdtGate(33, (uint32_t)irq1);
-    setIdtGate(34, (uint32_t)irq2);
-    setIdtGate(35, (uint32_t)irq3);
-    setIdtGate(36, (uint32_t)irq4);
-    setIdtGate(37, (uint32_t)irq5);
-    setIdtGate(38, (uint32_t)irq6);
-    setIdtGate(39, (uint32_t)irq7);
-    setIdtGate(40, (uint32_t)irq8);
-    setIdtGate(41, (uint32_t)irq9);
-    setIdtGate(42, (uint32_t)irq10);
-    setIdtGate(43, (uint32_t)irq11);
-    setIdtGate(44, (uint32_t)irq12);
-    setIdtGate(45, (uint32_t)irq13);
-    setIdtGate(46, (uint32_t)irq14);
-    setIdtGate(47, (uint32_t)irq15);
+    IDT::setIdtGate(32, (uint32_t)irq0);
+    IDT::setIdtGate(33, (uint32_t)irq1);
+    IDT::setIdtGate(34, (uint32_t)irq2);
+    IDT::setIdtGate(35, (uint32_t)irq3);
+    IDT::setIdtGate(36, (uint32_t)irq4);
+    IDT::setIdtGate(37, (uint32_t)irq5);
+    IDT::setIdtGate(38, (uint32_t)irq6);
+    IDT::setIdtGate(39, (uint32_t)irq7);
+    IDT::setIdtGate(40, (uint32_t)irq8);
+    IDT::setIdtGate(41, (uint32_t)irq9);
+    IDT::setIdtGate(42, (uint32_t)irq10);
+    IDT::setIdtGate(43, (uint32_t)irq11);
+    IDT::setIdtGate(44, (uint32_t)irq12);
+    IDT::setIdtGate(45, (uint32_t)irq13);
+    IDT::setIdtGate(46, (uint32_t)irq14);
+    IDT::setIdtGate(47, (uint32_t)irq15);
 
-    setIdt(); /* Load with ASM */
+    IDT::setIdt(); /* Load with ASM */
 }
 
 void isrHandler(registers_t r) {
