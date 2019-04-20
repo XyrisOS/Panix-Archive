56957f38a12ac7926aea26ece22175b5f66a4686
Keeton Feavel <me@kfeavel.com>
2019-04-19 20:26:03 -0400

    Added more shift characters. These need to be handled in the keyboard driver.

--- src/drivers/keyboard/KeyboardEventHandler.cpp
+++ src/drivers/keyboard/KeyboardEventHandler.cpp
@@ -18,13 +18,26 @@ void KeyboardEventHandler::onKeyDown(char c) {
         const char* cStr;
         // Handle special characters
         switch (c) {
-            case '1':
-                break;
+            case '1': printf("!"); break;
+            case '2': printf("@"); break;
+            case '3': printf("#"); break;
+            case '4': printf("$"); break;
+            case '5': printf("%"); break;
+            case '6': printf("^"); break;
+            case '7': printf("&"); break;
+            case '8': printf("*"); break;
+            case '9': printf("("); break;
+            case '0': printf(")"); break;
+            case '-': printf("_"); break;
+            case '=': printf("+"); break;
+            case '/': printf("?"); break;
+            case '.': printf(">"); break;
+            case ',': printf("<"); break;
             default:
                 cStr = toUpper(str);
+                printf(cStr);
                 break;
         }
-        printf(cStr);
     // print the lowercase version
     } else {
         const char str[2] = {c, '\0'};
