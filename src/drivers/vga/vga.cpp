acf1f6a49dec422f01f19ee801bc7deb9ec6e1d6
Keeton T. Feavel <keetonfeavel@gmail.com>
2019-08-29 18:23:10 -0400

    Created VGA constructor and destructor

--- src/drivers/vga/vga.cpp
+++ src/drivers/vga/vga.cpp
@@ -1,2 +1,21 @@
 #include <drivers/vga/vga.hpp>
 
+VideoGraphicsArray::VideoGraphicsArray() : 
+    miscPort(0x3c2),
+    crtcIndexPort(0x3d4),
+    crtcDataPort(0x3d5),
+    sequencerIndexPort(0x3c4),
+    sequencerDataPort(0x3c5),
+    graphicsControllerIndexPort(0x3ce),
+    graphicsControllerDataPort(0x3cf),
+    attributeControllerIndexPort(0x3c0),
+    attributeControllerReadPort(0x3c1),
+    attributeControllerWritePort(0x3c0),
+    attributeControllerResetPort(0x3da)
+{
+    //
+}
+
+VideoGraphicsArray::~VideoGraphicsArray() {
+    //
+}
