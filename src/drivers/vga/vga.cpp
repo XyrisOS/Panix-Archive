66a65c08f7ee6f6c5bdc6eb7e7ea252a7093996f
Keeton T. Feavel <keetonfeavel@gmail.com>
2019-08-29 21:14:50 -0400

    Fixed hex capitalization

--- src/drivers/vga/vga.cpp
+++ src/drivers/vga/vga.cpp
@@ -1,21 +1,21 @@
 #include <drivers/vga/vga.hpp>
 
 VideoGraphicsArray::VideoGraphicsArray() : 
-    miscPort(0x3c2),
-    crtcIndexPort(0x3d4),
-    crtcDataPort(0x3d5),
-    sequencerIndexPort(0x3c4),
-    sequencerDataPort(0x3c5),
-    graphicsControllerIndexPort(0x3ce),
-    graphicsControllerDataPort(0x3cf),
-    attributeControllerIndexPort(0x3c0),
-    attributeControllerReadPort(0x3c1),
-    attributeControllerWritePort(0x3c0),
-    attributeControllerResetPort(0x3da)
+    miscPort(0x3C2),
+    crtcIndexPort(0x3D4),
+    crtcDataPort(0x3D5),
+    sequencerIndexPort(0x3C4),
+    sequencerDataPort(0x3C5),
+    graphicsControllerIndexPort(0x3CE),
+    graphicsControllerDataPort(0x3CF),
+    attributeControllerIndexPort(0x3C0),
+    attributeControllerReadPort(0x3C1),
+    attributeControllerWritePort(0x3C0),
+    attributeControllerResetPort(0x3DA)
 {
     //
 }
 
 VideoGraphicsArray::~VideoGraphicsArray() {
-    //
+    
 }
