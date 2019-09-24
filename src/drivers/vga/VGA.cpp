c8593bf92a66c314f33d64d0d510984812a97652
Keeton T. Feavel <keetonfeavel@gmail.com>
2019-09-24 10:46:41 -0400

    Broke up onKeyDown into multiple handler functions

--- src/drivers/vga/VGA.cpp
+++ src/drivers/vga/VGA.cpp
@@ -111,11 +111,11 @@ uint8_t* VideoGraphicsArray::getFrameBufferSegment() {
 }
 
 uint8_t VideoGraphicsArray::getColorIndex(uint8_t r, uint8_t g, uint8_t b) {
-    if(r == 0x00 && g == 0x00 && b == 0x00) return 0x00; // black
-    if(r == 0x00 && g == 0x00 && b == 0xA8) return 0x01; // blue
-    if(r == 0x00 && g == 0xA8 && b == 0x00) return 0x02; // green
-    if(r == 0xA8 && g == 0x00 && b == 0x00) return 0x04; // red
-    if(r == 0xFF && g == 0xFF && b == 0xFF) return 0x3F; // white
+    if (r == 0x00 && g == 0x00 && b == 0x00) return 0x00; // black
+    if (r == 0x00 && g == 0x00 && b == 0xA8) return 0x01; // blue
+    if (r == 0x00 && g == 0xA8 && b == 0x00) return 0x02; // green
+    if (r == 0xA8 && g == 0x00 && b == 0x00) return 0x04; // red
+    if (r == 0xFF && g == 0xFF && b == 0xFF) return 0x3F; // white
     return 0x00;
 }
 
