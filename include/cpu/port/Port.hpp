1536313a5fcf0574618849de6441516381be8495
Keeton T. Feavel <keetonfeavel@gmail.com>
2019-09-25 14:12:16 -0400

    Cleaned up some of the port header and implementation syntax

--- include/cpu/port/Port.hpp
+++ include/cpu/port/Port.hpp
@@ -15,72 +15,46 @@ class Port8Bit : public Port {
     public:
         Port8Bit(uint16_t portnumber);
         //virtual ~Port8Bit();
-
         virtual uint8_t read();
         virtual void write(uint8_t data);
 
     protected:
-        static inline uint8_t read8(uint16_t _port) {
-            uint8_t result;
-            __asm__ volatile("inb %1, %0" : "=a" (result) : "Nd" (_port));
-            return result;
-        }
-
-        static inline void write8(uint16_t _port, uint8_t _data) {
-            __asm__ volatile("outb %0, %1" : : "a" (_data), "Nd" (_port));
-        }
+        static uint8_t read8(uint16_t _port);
+        static void write8(uint16_t _port, uint8_t _data);
 };
 
 class Port8BitSlow : public Port8Bit {
     public:
         Port8BitSlow(uint16_t portnumber);
         //virtual ~Port8BitSlow();
-
         virtual void write(uint8_t data);
+
     protected:
-        static inline void write8Slow(uint16_t _port, uint8_t _data) {
-            __asm__ volatile("outb %0, %1\njmp 1f\n1: jmp 1f\n1:" : : "a" (_data), "Nd" (_port));
-        }
+        static void write8Slow(uint16_t _port, uint8_t _data);
 };
 
 class Port16Bit : public Port {
     public:
         Port16Bit(uint16_t portnumber);
         //virtual ~Port16Bit();
-
         virtual uint16_t read();
         virtual void write(uint16_t data);
 
     protected:
-        static inline uint16_t read16(uint16_t _port) {
-            uint16_t result;
-            __asm__ volatile("inw %1, %0" : "=a" (result) : "Nd" (_port));
-            return result;
-        }
-
-        static inline void write16(uint16_t _port, uint16_t _data) {
-            __asm__ volatile("outw %0, %1" : : "a" (_data), "Nd" (_port));
-        }
+        static uint16_t read16(uint16_t _port);
+        static void write16(uint16_t _port, uint16_t _data);
 };
 
 class Port32Bit : public Port {
     public:
         Port32Bit(uint16_t portnumber);
         //virtual ~Port32Bit();
-
         virtual uint32_t read();
         virtual void write(uint32_t data);
 
     protected:
-        static inline uint32_t read32(uint16_t _port) {
-            uint32_t result;
-            __asm__ volatile("inl %1, %0" : "=a" (result) : "Nd" (_port));
-            return result;
-        }
-
-        static inline void write32(uint16_t _port, uint32_t _data) {
-            __asm__ volatile("outl %0, %1" : : "a"(_data), "Nd" (_port));
-        }
+        static uint32_t read32(uint16_t _port);
+        static void write32(uint16_t _port, uint32_t _data);
 };
 
 #endif /* PANIX_PORT_HPP */
--- src/cpu/port/Port.cpp
+++ src/cpu/port/Port.cpp
@@ -2,32 +2,40 @@
 #include <cpu/port/Port.hpp>
 
 /* Port */
-Port::Port(uint16_t portnumber)
-{
+Port::Port(uint16_t portnumber) {
     this->portnumber = portnumber;
 }
 /*
-Port::~Port()
-{
+Port::~Port() {
+    
 }
 */
 /* Port 8-Bit */
-Port8Bit::Port8Bit(uint16_t portnumber)
-    : Port(portnumber)
+Port8Bit::Port8Bit(uint16_t portnumber) 
+    : Port(portnumber) 
 {
+    // Stubbed
 }
 /*
-Port8Bit::~Port8Bit()
-{
+Port8Bit::~Port8Bit() {
+    
 }
 */
-void Port8Bit::write(uint8_t data)
-{
+inline uint8_t Port8Bit::read8(uint16_t _port) {
+    uint8_t result;
+    __asm__ volatile("inb %1, %0" : "=a" (result) : "Nd" (_port));
+    return result;
+}
+
+inline void Port8Bit::write8(uint16_t _port, uint8_t _data) {
+    __asm__ volatile("outb %0, %1" : : "a" (_data), "Nd" (_port));
+}
+
+void Port8Bit::write(uint8_t data) {
     write8(portnumber, data);
 }
 
-uint8_t Port8Bit::read()
-{
+uint8_t Port8Bit::read() {
     return read8(portnumber);
 }
 
@@ -35,35 +43,48 @@ uint8_t Port8Bit::read()
 Port8BitSlow::Port8BitSlow(uint16_t portnumber)
     : Port8Bit(portnumber)
 {
+    // Stubbed
 }
 /*
-Port8BitSlow::~Port8BitSlow()
-{
+Port8BitSlow::~Port8BitSlow() {
+
 }
 */
-void Port8BitSlow::write(uint8_t data)
-{
+
+inline void Port8BitSlow::write8Slow(uint16_t _port, uint8_t _data) {
+    __asm__ volatile("outb %0, %1\njmp 1f\n1: jmp 1f\n1:" : : "a" (_data), "Nd" (_port));
+}
+
+void Port8BitSlow::write(uint8_t data) {
     write8Slow(portnumber, data);
 }
 
 /* Port 16-Bit */
-
 Port16Bit::Port16Bit(uint16_t portnumber)
     : Port(portnumber)
 {
+    // Stubbed
 }
 /*
-Port16Bit::~Port16Bit()
-{
+Port16Bit::~Port16Bit() {
+
 }
 */
-void Port16Bit::write(uint16_t data)
-{
+inline uint16_t Port16Bit::read16(uint16_t _port) {
+    uint16_t result;
+    __asm__ volatile("inw %1, %0" : "=a" (result) : "Nd" (_port));
+    return result;
+}
+
+inline void Port16Bit::write16(uint16_t _port, uint16_t _data) {
+    __asm__ volatile("outw %0, %1" : : "a" (_data), "Nd" (_port));
+}
+
+void Port16Bit::write(uint16_t data) {
     write16(portnumber, data);
 }
 
-uint16_t Port16Bit::read()
-{
+uint16_t Port16Bit::read() {
     return read16(portnumber);
 }
 
@@ -71,18 +92,27 @@ uint16_t Port16Bit::read()
 Port32Bit::Port32Bit(uint16_t portnumber)
     : Port(portnumber)
 {
+    // Stubbed
 }
 /*
-Port32Bit::~Port32Bit()
-{
+Port32Bit::~Port32Bit() {
+
 }
 */
-void Port32Bit::write(uint32_t data)
-{
+inline uint32_t Port32Bit::read32(uint16_t _port) {
+    uint32_t result;
+    __asm__ volatile("inl %1, %0" : "=a" (result) : "Nd" (_port));
+    return result;
+}
+
+inline void Port32Bit::write32(uint16_t _port, uint32_t _data) {
+    __asm__ volatile("outl %0, %1" : : "a"(_data), "Nd" (_port));
+}
+
+void Port32Bit::write(uint32_t data) {
     write32(portnumber, data);
 }
 
-uint32_t Port32Bit::read()
-{
+uint32_t Port32Bit::read() {
     return read32(portnumber);
 }
