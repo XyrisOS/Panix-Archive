ec06ec8bd2da66ce85fd4db56e253fc7fc52f2ae
Keeton T. Feavel <keetonfeavel@gmail.com>
2019-09-25 14:19:36 -0400

    Removed unused destructor

--- src/cpu/port/Port.cpp
+++ src/cpu/port/Port.cpp
@@ -16,19 +16,15 @@ Port8Bit::Port8Bit(uint16_t portnumber)
 {
     // Stubbed
 }
-/*
-Port8Bit::~Port8Bit() {
-    
-}
-*/
-inline uint8_t Port8Bit::read8(uint16_t _port) {
+
+inline uint8_t Port8Bit::read8(uint16_t port) {
     uint8_t result;
-    __asm__ volatile("inb %1, %0" : "=a" (result) : "Nd" (_port));
+    __asm__ volatile("inb %1, %0" : "=a" (result) : "Nd" (port));
     return result;
 }
 
-inline void Port8Bit::write8(uint16_t _port, uint8_t _data) {
-    __asm__ volatile("outb %0, %1" : : "a" (_data), "Nd" (_port));
+inline void Port8Bit::write8(uint16_t port, uint8_t data) {
+    __asm__ volatile("outb %0, %1" : : "a" (data), "Nd" (port));
 }
 
 void Port8Bit::write(uint8_t data) {
@@ -45,14 +41,9 @@ Port8BitSlow::Port8BitSlow(uint16_t portnumber)
 {
     // Stubbed
 }
-/*
-Port8BitSlow::~Port8BitSlow() {
 
-}
-*/
-
-inline void Port8BitSlow::write8Slow(uint16_t _port, uint8_t _data) {
-    __asm__ volatile("outb %0, %1\njmp 1f\n1: jmp 1f\n1:" : : "a" (_data), "Nd" (_port));
+inline void Port8BitSlow::write8Slow(uint16_t port, uint8_t data) {
+    __asm__ volatile("outb %0, %1\njmp 1f\n1: jmp 1f\n1:" : : "a" (data), "Nd" (port));
 }
 
 void Port8BitSlow::write(uint8_t data) {
@@ -65,19 +56,15 @@ Port16Bit::Port16Bit(uint16_t portnumber)
 {
     // Stubbed
 }
-/*
-Port16Bit::~Port16Bit() {
 
-}
-*/
-inline uint16_t Port16Bit::read16(uint16_t _port) {
+inline uint16_t Port16Bit::read16(uint16_t port) {
     uint16_t result;
-    __asm__ volatile("inw %1, %0" : "=a" (result) : "Nd" (_port));
+    __asm__ volatile("inw %1, %0" : "=a" (result) : "Nd" (port));
     return result;
 }
 
-inline void Port16Bit::write16(uint16_t _port, uint16_t _data) {
-    __asm__ volatile("outw %0, %1" : : "a" (_data), "Nd" (_port));
+inline void Port16Bit::write16(uint16_t port, uint16_t data) {
+    __asm__ volatile("outw %0, %1" : : "a" (data), "Nd" (port));
 }
 
 void Port16Bit::write(uint16_t data) {
@@ -94,19 +81,15 @@ Port32Bit::Port32Bit(uint16_t portnumber)
 {
     // Stubbed
 }
-/*
-Port32Bit::~Port32Bit() {
 
-}
-*/
-inline uint32_t Port32Bit::read32(uint16_t _port) {
+inline uint32_t Port32Bit::read32(uint16_t port) {
     uint32_t result;
-    __asm__ volatile("inl %1, %0" : "=a" (result) : "Nd" (_port));
+    __asm__ volatile("inl %1, %0" : "=a" (result) : "Nd" (port));
     return result;
 }
 
-inline void Port32Bit::write32(uint16_t _port, uint32_t _data) {
-    __asm__ volatile("outl %0, %1" : : "a"(_data), "Nd" (_port));
+inline void Port32Bit::write32(uint16_t port, uint32_t data) {
+    __asm__ volatile("outl %0, %1" : : "a"(data), "Nd" (port));
 }
 
 void Port32Bit::write(uint32_t data) {
